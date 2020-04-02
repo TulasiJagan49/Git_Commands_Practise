7e49d86ae7f9448640852714252036dd216a22cd
madhulika9293 <ramyamadhulika9293@msitprogram.net>
2020-04-02 13:03:10 +0530

    changes

--- project1/import.py
+++ project1/import.py
@@ -1,8 +1,7 @@
 import os
 import csv
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -11,12 +10,10 @@ engine= create_engine(os.getenv("DATABASE_URL"))
 db =scoped_session(sessionmaker(bind=engine))
 
 def main():
-    # db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
-    # db.execute("CREATE TABLE reviews (isbn VARCHAR NOT NULL,review VARCHAR NOT NULL, rating INTEGER NOT NULL,username VARCHAR NOT NULL)")
-    db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year INTEGER NOT NULL)")
+    db.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, isbn VARCHAR NOT NULL,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year INTEGER NOT NULL)")
     f=open("books.csv")
     reader =csv.reader(f)
-    headers = next(reader)
+    headers = reader.next()
     for isbn,title,author,year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:a,:b,:c,:d)",{"a":isbn,"b":title,"c":author,"d":year})
         
