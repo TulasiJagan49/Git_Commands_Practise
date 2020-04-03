ddba6c85de60aa65e4f6b27842aa84ccb0c73ac2
madhulika9293 <ramyamadhulika9293@msitprogram.net>
2020-04-03 15:58:56 +0530

    initial

--- project1/import.py
+++ project1/import.py
@@ -1,18 +1,20 @@
 import os, csv
 
-from objects import Book, db
+from objects import Book
+from application import db
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 def main():
+    # db.create_all()
     f=open("books.csv")
     reader =csv.reader(f)
     headers = next(reader)
     for isbn,title,author,year in reader:
         db.session.add(Book(isbn,title,author,year))        
     print("done")            
-    db.commit()
+    db.session.commit()
 
 if __name__ == "__main__":
     main()
